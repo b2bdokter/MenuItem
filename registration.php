9d8a2205b96caf5fd3372b2218aba02540f7b263
Ruud Weijters <ruud.weijters@outlook.com>
2022-01-22 17:08:44 +0100

    versie1

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright RW 2022
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'RW_MenuItem',
+    __DIR__
+);
+
